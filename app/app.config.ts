0cdd3908481346e85758a53934ebd15996496e1d
Aidil Aryanto <aidilarynto@gmail.com>
2025-07-02 12:13:36 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,9 +4,8 @@ export default defineAppConfig({
   github: 'https://github.com/aidilaryanto/sink',
   twitter: 'https://s.aidilaryanto.id/x',
   telegram: 'https://s.aidilaryanto.id/telegram',
-  mastodon: 'https://sink.cool/mastodon',
   blog: 'https://s.aidilaryanto.id/portfolio',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'A Simple / Speedy / Secure Link Shortener.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
